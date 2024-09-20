020eb2761e75fb32e76cca03719ed4e51d4f36fa
Ryan You <115812707+bbysitterr@users.noreply.github.com>
2024-09-20 10:29:38 -0700

    created Init method
    plsplsplsplsplsplspsls commit plspslspls

--- Git.java
+++ Git.java
@@ -1,5 +1,35 @@
+
+import java.io.File;
+
 public class Git {
+
     public static void main(String[] args) {
-        
+
+    }
+
+    public void init() {
+
+        String gitDirPath = "git";
+        String objectsDirPath = gitDirPath + "/objects";
+        String indexFilePath = gitDirPath + "/index";
+
+        File gitDir = new File(gitDirPath);
+        File objects = new File(objectsDirPath);
+        File index = new File(indexFilePath);
+        if (gitDir.exists()) {
+            System.out.println("Git Repository already exists");
+        } else {
+            gitDir.mkdir();
+            if (objects.exists()) {
+                System.out.println("Objects file already exists");
+            } else {
+                gitDir.mkdir();
+            }
+            if (index.exists()) {
+                System.out.println("Index file already exists");
+            } else {
+                gitDir.mkdir();
+            }
+        }
     }
 }
